8ab72e2656de1e099dc5f8a8e71e08b5e497d3da
Himeshtanti <tanti7002@gmail.com>
2024-03-02 15:58:57 +0530

    first commit

---
+++ java script/main.js
@@ -0,0 +1,97 @@
+// console.log("Hello,"+ "Bharat")
+
+// let player1 = '47';
+// let player2 = player1;
+
+// console.log(player1 === player2)
+
+// console.log(typeof(player1 = player2))
+
+// let person1 = 51;
+
+// person1 -= 6
+
+// console.log(person1)
+
+// let num1 = 19;
+// let num2 = 2;
+
+// console.log(num2/num1)
+
+// let myname = 'sidhart tanti';
+// let yourname = "kabir kumar tanti"
+
+// console.log(myname.length)
+// console.log(yourname.charAt(0))
+// console.log(myname.indexOf("s"))
+
+// console.log(yourname.slice(0,1))
+// console.log(yourname.toLocaleUpperCase())
+// console.log(yourname.includes())
+// console.log(myname.split(' '))
+// console.log(yourname.split(' '))
+
+// let age = "23";
+
+// console.log(typeof age)
+
+// let numberage = Number(age)
+
+// console.log(typeof Number(age))
+
+// let marks = 90.38547468746
+
+// console.log(marks.toFixed(2))
+
+// console.log(Math.PI)
+
+// console.log(Math.E)
+
+// console.log(Math.trunc(7.9))
+
+// console.log(Math.ceil(8.1))
+
+// console.log(Math.pow(3,4))
+
+// console.log(Math.max(6,9))
+
+// console.log(Math.min(9,1))
+
+// console.log(Math.random()*9)
+
+const person1 ={
+    name: "Bharat",
+    age: 29,
+    address: {city: "chennai", state: "Tamil Nadu", Town: "kaivellai", street: "4th cross"}
+}
+
+console.log = {}
+
+class book{
+    constructor(hospital, patient, madicine, price){
+        this.hospital = CMC;
+        this.patient = Sohil;
+        this.madicine = zinccotine, parajasic;
+        this.price = 110;
+    }
+
+    showhospital(){
+        return 'hospital of $ {this.name} is $ {this.price}'
+    }
+}
+
+const hospital1 = CMC("medicine","faaruk", 110);
+const hospital2 = Apollo("madicine","Sohil", 150);
+const hospital3 = tamil("madicine", "tanwir", 250);
+
+console.log(hospital1.showname());
+console.log(hospital2.showprice());
+console.log(hospital3.shomadicine());
+
+
+
+
+
+
+
+const posts =["post1", "post2","post3"]
